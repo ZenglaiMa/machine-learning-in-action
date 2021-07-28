29ca0a8f4d4cb6f57887d36d3dc12604a398bfc8
马增来 <1303035373@qq.com>
2021-07-28 23:22:57 +0800

    add normalization

--- 1-kNN/2_use-kNN-to-improve-dating-website/data_process.py
+++ 1-kNN/2_use-kNN-to-improve-dating-website/data_process.py
@@ -17,12 +17,11 @@ def get_data(filename):
             elif line[-1] == 'largeDoses':
                 labels.append(3)
             index += 1
-    return normalization(feature_mat), labels
+    return feature_mat, labels
 
 
-# 归一化 (X-X_min)/(X_max-X_min) 或 (X-X_mean)/X_std
+# 归一化 (X-X_min)/(X_max-X_min)
 def normalization(feature_mat):
     x_min = np.min(feature_mat, axis=0)
     x_max = np.max(feature_mat, axis=0)
-    return (feature_mat - x_min) / (x_max - x_min)
-    # return (feature_mat - np.mean(feature_mat, axis=0)) / np.std(feature_mat, axis=0)
+    return (feature_mat - x_min) / (x_max - x_min), x_min, (x_max - x_min)
--- 1-kNN/2_use-kNN-to-improve-dating-website/predict.py
+++ 1-kNN/2_use-kNN-to-improve-dating-website/predict.py
@@ -2,7 +2,7 @@ import numpy as np
 import os
 
 from kNN import kNN
-from data_process import get_data
+from data_process import get_data, normalization
 
 
 def predict():
@@ -11,11 +11,13 @@ def predict():
     percent_video_games = float(input("玩视频游戏所消耗时间百分比: "))
     fly_miles = float(input("每年获得的飞行常客里程数: "))
     ice_cream = float(input("每周消费的冰淇淋公升数: "))
+
     input_x = np.array([percent_video_games, fly_miles, ice_cream])
 
     feature_mat, labels = get_data(os.path.join('./data', 'datingTestSet.txt'))
+    feature_mat, x_min, x_range = normalization(feature_mat)  # 归一化
 
-    result = kNN(input_x, feature_mat, labels, 4)
+    result = kNN((input_x - x_min) / x_range, feature_mat, labels, 4)  # 测试数据也要归一化
     print('你可能%s这个人!' % (result_list[result - 1]))
 
 
